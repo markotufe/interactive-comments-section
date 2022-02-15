80dcd171eeadda44517ebdfeba6330366fe97639
Marko Tufegdzic <marko@hireapp.me>
2022-02-15 23:33:26 +0100

    Auth started...

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <h1>Login to see content</h1>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    submitForm() {
+      console.log("forma je potvrdjena");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Hello, I'm Login!</h1>
+  <div>
+    <h1>Hello, I'm Login!</h1>
+    <login-form></login-form>
+  </div>
 </template>
+
+<script>
+import LoginForm from "../components/auth/LoginForm.vue";
+
+export default {
+  components: {
+    LoginForm,
+  },
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+</style>
