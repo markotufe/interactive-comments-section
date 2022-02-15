f1b5c3579372909a6bc43d0ae0f908ce57627afa
Marko Tufegdzic <marko@hireapp.me>
2022-02-15 13:37:59 +0100

    Not found route

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,1 +1,28 @@
-<template>Route not found</template>
+<template>
+  <div>
+    <h1>Route not found</h1>
+    <router-link to="/">Go to home</router-link>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+
+h1 {
+  margin-bottom: 10px;
+  font-size: 25px;
+}
+
+a {
+  text-decoration: none;
+  font-size: 17px;
+  color: var(--primary-color);
+  font-weight: bold;
+}
+</style>
