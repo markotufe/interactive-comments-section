ac66f2e0b3b2adf51c4f078b812f4ff056bff313
Marko Tufegdzic <marko@hireapp.me>
2022-02-16 21:39:55 +0100

    Style fixes

--- src/App.vue
+++ src/App.vue
@@ -22,42 +22,6 @@ export default {
 
       unsub();
     });
-    console.log(this.$store.getters.getGlobalState);
   },
 };
 </script>
-
-<style>
-#app {
-  background-color: var(--very-light-gray);
-  height: 100vh;
-}
-
-.container {
-  width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-}
-@media (min-width: 576px) {
-  .container {
-    width: 540px;
-  }
-}
-@media (min-width: 768px) {
-  .container {
-    width: 720px;
-  }
-}
-@media (min-width: 992px) {
-  .container {
-    width: 960px;
-  }
-}
-@media (min-width: 1200px) {
-  .container {
-    width: 1140px;
-  }
-}
-</style>
